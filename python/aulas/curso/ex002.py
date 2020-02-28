32cd1a3b08506a8bbdf2a2ed3efeeb7c00800e4b
joaopaulog90 <joaopaulo-g90@hotmail.com>
2020-02-27 22:27:42 -0300

    teste

---
+++ python/aulas/curso/ex002.py
@@ -0,0 +1,5 @@
+nome = input('digite seu nome')
+
+j1 = 'teste1'
+j2 = 'teste2'
+print('é um prazer {} , {} ,{}'.format(nome, j1, j2))
---
+++ python/aulas/curso/tiposPrimitivos.py
@@ -0,0 +1,5 @@
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite um valor: '))
+s = n1+n2
+#print('a soma entre ',n1 ,' e ',n2,' é ',s) tipo de formatação nao utilizado
+print('a soma entre {} e {} é {}'.format(n1, n2, s)) #utlizado a partir do python3
